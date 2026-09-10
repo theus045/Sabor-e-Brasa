d90a6c893d7cbca253bf9b57e1fd43f016afc2f7
Matheus Soares <ms4458475@gmail.com>
2026-09-09 23:31:45 -0300

    Primeira versão do site Sabor e Brasa

---
+++ sabor e brasa/js/script.js
@@ -0,0 +1,27 @@
+const cards = document.querySelectorAll(".menu-item");
+
+const observer = new IntersectionObserver((entries) => {
+
+    entries.forEach((entry) => {
+
+        if (entry.isIntersecting) {
+            entry.target.classList.add("show");
+        }
+
+    });
+
+}, {
+    threshold: 0.2
+});
+
+cards.forEach((card) => {
+    observer.observe(card);
+});
+
+/* nav */
+const menuToggle = document.querySelector(".menu-toggle");
+const navLinks = document.querySelector(".nav-links");
+
+menuToggle.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+});
